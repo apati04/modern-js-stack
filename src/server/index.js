8067e4baadf45e0c6d34c6bae75aaff58d49d3ed
Andrew Patipaksiri <apax714@gmail.com>
2017-05-14 09:49:24 -0700

    express server

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 // @flow
 
-import comporession from 'compression'
+import compression from 'compression'
 import express from 'express'
 
 import { APP_NAME, STATIC_PATH, WEB_PORT } from '../shared/config'
@@ -10,10 +10,10 @@ import renderApp from './render-app'
 const app = express()
 
 app.use(compression())
-app.use(STATIC_PATH, express.stateic('dist'))
+app.use(STATIC_PATH, express.static('dist'))
 app.use(STATIC_PATH, express.static('public'))
 
-app.get ('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send(renderApp(APP_NAME))
 })
 
